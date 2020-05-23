b5471723daa5fe41fdf64631ce5140ad7810d0d4
Paul Evans <decretist@gmail.com>
2020-05-22 19:58:00 -0700

    Fix markdown table generation

--- report.py
+++ report.py
@@ -4,7 +4,7 @@ import pandas as pd
 def main():
     df = pd.read_csv('./bug-reports.csv')
     f = open('./errors.md', 'w')
-    f.write(df[['Citation','Column','Error','Correction']].to_markdown())
+    f.write(df[['Citation','Column','Error','Correction']].to_markdown(showindex=False))
     f.close()
 
 if __name__ == '__main__':
